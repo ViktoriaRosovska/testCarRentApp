f37c827abb6b429a62bcc7c1f161f8fa991a2415
Viktoria Rosovska <torimeister@gmail.com>
2023-11-15 22:40:04 +0200

    add config

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
       <h1>React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
